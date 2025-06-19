0a45e10b3419c6cba714f9104a74e2d4ecc2b637
patrikcath <patrikbinder@gmail.com>
2025-06-19 03:05:00 +0100

    Update rollsimulator.py
    fixed up the code a bit

--- rollsimulator.py
+++ rollsimulator.py
@@ -1,102 +1,100 @@
 #@patrikcath
-#not my best work but it gets the job done and I ran out of bugs to fix. Cleaning up the code and extra functionality will come soon.
 
 #imports
 from tkinter import Tk, Label, Button, Entry, W, PhotoImage, TclError
 from random import seed, randint
 from time import time
 
-#set up modules
-seed(time)
-root = Tk()
-root.title("Dice simulator")
+def setup():
+    #seed the RNGenerator
+    seed(time)
 
-#setting the program's icon is inside a try-except to make it marginally easier to share the code. It'll be changed in a future version
-try:
-    icon = PhotoImage(file="icon.png")
-    root.iconphoto(False,icon)
-except TclError:#TclError happens when a file does not exist
-    print("Missing icon.png")
+    #set up tkinter
+    root = Tk()
+    root.title("Dice simulator")
+
+    #attempt to set the program's icon
+    try:
+        icon = PhotoImage(file="icon.png")
+        root.iconphoto(False,icon)
+    except TclError:#TclError happens when a file does not exist
+        print("Icon file not found")
+
+    return root
+
+def gridLabel(inRow, inColumn, inColumnspan=1, inText="", inSticky=W):
+    #creates a label and places it using tkinter's grid system
+    label = Label(root, text=inText)
+    label.grid(row=inRow, column=inColumn, columnspan=inColumnspan, sticky=inSticky)
+    return label
+
+def gridEntry(inRow, inColumn, inDefault, inSticky=W):
+    #creates an entry widget and places it using tkinter's grid system
+    entry = Entry(root)
+    entry.grid(row=inRow, column=inColumn,sticky=inSticky)
+    entry.insert(0, inDefault)
+    return entry
 
 def generate():
-    #roll a dice with a specified amount of sides a specified number of times, then apply modifiers and advantages
-    rolls = []
+    #roll a dice with a specified amount of sides a specified number of times, then apply modifiers and advantages and display the result
     try:
-        #get inputs
-        numberOfDice = int(diceInputEntry.get())
-        sidesPerDice = int(sidesInputEntry.get())
-        modifier = int(modifierInputEntry.get())
-        advantage = int(advantageInputEntry.get())
-
         #roll the dice
-        for i in range(numberOfDice+abs(advantage)):
-            diceRoll = randint(1,sidesPerDice)
-            rolls.append(diceRoll)
+        rolls = [1] * (int(diceInputEntry.get()) + abs(int(advantageInputEntry.get())))
+        for i in range(len(rolls)):
+            rolls[i] = randint(1, int(sidesInputEntry.get()))
 
-        #display the rolls
+        #display the dice rolls
         rollsLabel.configure(text=f"Rolls: {str(rolls)[1:-1]}")
 
         #process advantage, display what the dis/advantage is if there is one
-        #advantage adds extra dice to the roll. positive advantage removes the lowest dice of the roll, negative advantage removes the highest dice
-        if advantage > 0:#positive advantage
-            advantageLabel.configure(text=f"Rolling with a +{advantage} advantage")
-            for i in range(0,advantage):
+        if int(advantageInputEntry.get()) > 0:#if there's a advantage, remove n lowest rolls from the pool, where n is equivalent to the level of advantage
+            advantageLabel.configure(text=f"Rolling with a +{int(advantageInputEntry.get())} advantage")
+            for i in range(0,int(advantageInputEntry.get())):
                 rolls.pop(rolls.index(min(rolls)))
-        elif advantage < 0:#disadvantage
-            advantageLabel.configure(text=f"Rolling with a {advantage} disadvantage")
-            for i in range(0,advantage):
+        elif int(advantageInputEntry.get()) < 0:#if there's a disadvantage, remove n HIGHEST rolls from the pool, where n is equivalent to the level of disadvantage
+            advantageLabel.configure(text=f"Rolling with a {int(advantageInputEntry.get())} disadvantage")
+            for i in range(0,int(advantageInputEntry.get())):
                 rolls.pop(rolls.index(max(rolls)))
-        else:#no dis/advantage
+        else:#if there's no advantage or disadvantage don't display anything
             advantageLabel.configure(text="")
 
         #display and apply the modifier
-        #modifier adds or subtracts a value from the final result
-        if modifier > 0:
-            modifierLabel.configure(text=f"Modifier: +{modifier}")
-        elif modifier < 0:
-            modifierLabel.configure(text=f"Modifier: {modifier}")
+        #a modifier adds or subtracts a value from the final result
+        if int(modifierInputEntry.get()) > 0:
+            modifierLabel.configure(text=f"Modifier: +{int(modifierInputEntry.get())}")
+        elif int(modifierInputEntry.get()) < 0:
+            modifierLabel.configure(text=f"Modifier: {int(modifierInputEntry.get())}")
         else: modifierLabel.configure(text="")
-        rolls.append(modifier)
+        rolls.append(int(modifierInputEntry.get()))
         
         #display the result
         totalLabel.configure(text=f"Total: {sum(rolls)}")
 
-    except ValueError:#should only come up if one of the inputs isn't a number
-        pass#nothing happens - this line only exists because try statements require exception handling
+    except ValueError:
+        print("Failed to generate dice pool: one or more of the inputs isn't a number")
 
+root = setup()
 
-#generate and place labels
-Label(root, text="Number of dice: ").grid(row=0, column=0,sticky=W)
-Label(root, text="Sides: ").grid(row=1, column=0,sticky=W)
-Label(root, text="Modifier: ").grid(row=2,column=0,sticky=W)
-Label(root, text="Advantage: ").grid(row=3,column=0,sticky=W)
+#label widgets to label the entry fields
+gridLabel(0, 0, inText="Number of dice: ")
+gridLabel(1, 0, inText="Sides: ")
+gridLabel(2, 0, inText="Modifier: ")
+gridLabel(3, 0, inText="Advantage: ")
 
-#generate and place user inputs and set defaults
-diceInputEntry = Entry(root)
-diceInputEntry.grid(row=0, column=1,sticky=W)
-diceInputEntry.insert(0,1)
-sidesInputEntry = Entry(root)
-sidesInputEntry.grid(row=1,column=1,sticky=W)
-sidesInputEntry.insert(0,20)
-modifierInputEntry = Entry(root)
-modifierInputEntry.grid(row=2,column=1,sticky=W)
-modifierInputEntry.insert(0,0)
-advantageInputEntry = Entry(root)
-advantageInputEntry.grid(row=3,column=1,sticky=W)
-advantageInputEntry.insert(0,0)
+#entry widgets to take user inputs
+diceInputEntry = gridEntry(0, 1, 1)
+sidesInputEntry = gridEntry(1, 1, 20)
+modifierInputEntry = gridEntry(2, 1, 0)
+advantageInputEntry = gridEntry(3, 1, 0)
 
-#generate and place the roll button
-generateButton = Button(root, text="Roll",command=generate).grid(row=4, column=0,columnspan=2,ipadx=10,ipady=5,pady=10)
+#button widget to generate the dice pool (doesn't get a function because there's just one)
+generateButton = Button(root, text="Roll",command=generate).grid(row=4, column=0, columnspan=2, ipadx=10, ipady=5, pady=10)
 
-#the output labels used to be initialised inside generate(), but that caused some really strange graphical bugs.
-#the labels have high columnspans to stop columns from unintentionally shifting around
-advantageLabel = Label(root)
-advantageLabel.grid(row=5,column=0,sticky=W,columnspan=999)
-modifierLabel = Label(root)
-modifierLabel.grid(row=6,column=0,sticky=W,columnspan=999)
-rollsLabel = Label(root)
-rollsLabel.grid(row=7,column=0,sticky=W,columnspan=999)
-totalLabel = Label(root)
-totalLabel.grid(row=8,column=0,sticky=W,columnspan=999)
+#label widgets to show the outputs
+#the column span for these is very wide to stop them from affecting the width of the entire column.
+advantageLabel = gridLabel(5, 0, 999)
+modifierLabel = gridLabel( 6, 0, 999)
+rollsLabel = gridLabel(7, 0, 999)
+totalLabel = gridLabel(8, 0, 999)
 
 root.mainloop()
